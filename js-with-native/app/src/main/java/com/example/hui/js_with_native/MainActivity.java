5957574a7f0f755b6bc2e503d83b297b13bccc2b
huigege1943 <807693217@qq.com>
2017-03-10 17:04:18 +0800

    add: 视图 Main

--- js-with-native/app/src/main/java/com/example/hui/js_with_native/MainActivity.java
+++ js-with-native/app/src/main/java/com/example/hui/js_with_native/MainActivity.java
@@ -1,13 +1,98 @@
 package com.example.hui.js_with_native;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+    private OkHttpClient client;
+    private String str="";
+    private String link = "http://www.weather.com.cn/data/cityinfo/101010100.html";
+    private Call call;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.mainWebView);
+        webView.setWebChromeClient(new WebChromeClient());
+        final WebSettings settings = webView.getSettings();
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setSupportZoom(true);
+
+        webView.addJavascriptInterface(new JavascriptInterface(), "jsInterface2");
+        try {
+            webView.loadUrl("file:///android_asset/main.html");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public class JavascriptInterface {
+
+        @android.webkit.JavascriptInterface
+        public void toSetting() {
+            Intent intent = new Intent(MainActivity.this,SettingActivity.class);
+            startActivity(intent);
+        }
+
+        @android.webkit.JavascriptInterface
+        public void api() throws IOException {
+            getAPI();
+        }
+    }
+
+    public void getAPI() throws IOException{
+        client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(link)
+                .addHeader("Accept", "application/json; q=0.5")
+                .build();
+        call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                str = response.body().string();
+                System.out.println(str);
+
+                sendPost();
+            }
+        });
+    }
+
+    public void sendPost(){
+        webView.post(new Runnable() {
+            @Override
+            public void run() {
+                if(str.equals("")){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else if (!str.equals("")) {
+                    webView.loadUrl("javascript:getLink(\'" + link + "\')");
+                    webView.loadUrl("javascript:getContent(\'" + str + "\')");
+                }
+            }
+        });
     }
 }
